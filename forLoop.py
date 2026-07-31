a87dcc9f238a4bc7d9e8ab2bdefef9286a5ccc3c
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:07:53 -0400

    Add for loops for printing numbers and multiplication

---
+++ forLoop.py
@@ -0,0 +1,8 @@
+for i in range(10): # i will start from 0 and go up to 9
+    print(i)
+
+n = int(input("Enter a number: "))
+for j in range(1, 11):
+    ans = n*j
+    print(ans)
+print("outside loop")
