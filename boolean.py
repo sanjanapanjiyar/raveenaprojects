c06c9fc55c08d07f30eb4af4e71d6c2ea168f47e
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:04:53 -0400

    Add boolean comparison operations in boolean.py

---
+++ boolean.py
@@ -0,0 +1,18 @@
+a = 10
+b = 80
+
+n=b>a
+print(b,">",a,"=",n)
+
+#Operators are >,<,>=,<=,==,!=
+
+n=b<a
+print(b,"<",a,"=",n)
+n=b>=a
+print(b,">=",a,"=",n)
+n=b<=a
+print(b,"<=",a,"=",n)
+n=b==a
+print(b,"==",a,"=",n)
+n=b!=a
+print(b,"!=",a,"=",n)
