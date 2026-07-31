931246fa5ea53dfb06a471c97d399a7f1df681ce
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:13:18 -0400

    Add multi-input demo for name, address, and phone

---
+++ multiInputDemo.py
@@ -0,0 +1,6 @@
+print("Enter your name, address, and phone")
+name=input("Enter your name:")
+address=input("Enter your address:")
+phone=int(input("Enter your phone number:"))
+
+print("Name is" , name+" Address is", address+" Phone number is", phone)
