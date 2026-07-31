b0972013fa85f57add9ccb88f8a7c071d1e3c1a2
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:08:37 -0400

    Add grading functionality based on input score

---
+++ grading.py
@@ -0,0 +1,11 @@
+grade=float(input("Enter your grade: "))
+if grade>=70:
+    print("Your grade is A")
+elif grade>=60:
+    print("Your grade is B") 
+elif grade>=50:
+    print("Your grade is C")
+elif grade>=40:
+    print("Your grade is D")
+else:
+    print("You failed")
