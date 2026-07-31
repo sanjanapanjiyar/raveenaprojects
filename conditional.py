fa3f25167f5318ce589d6e5134370eaf60d79e7b
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:05:13 -0400

    Add conditional subtraction program

---
+++ conditional.py
@@ -0,0 +1,8 @@
+a=int(input("Enter number "))
+b=int(input("Enter number "))
+if(a>b):
+    print("in if")
+    print("Subtraction is" , a-b)
+else:
+   print("in else")
+   print("Subtraction is" , b-a)
