91e12a6ea8d6e0c2575ce16ff852a6d826617f45
sanjanapanjiyar <sanjanapanjiyar@gmail.com>
2026-07-31 11:07:09 -0400

    Add script to calculate average of four numbers

---
+++ division.py
@@ -0,0 +1,6 @@
+a=float(input("Enter first number: "))
+b=float(input("Enter second number: "))
+c=float(input("Enter third number: "))
+d=float(input("Enter fourth number: "))
+result=(a+b+c+d)/4
+print("Average of four numbers is", result)
